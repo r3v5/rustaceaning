8b6721e35ad92c2d68defec79c0446dc533ad415
Ian Miller <milleryan2003@gmail.com>
2026-06-30 17:00:13 +0100

    feat: set up project
    Signed-off-by: Ian Miller <milleryan2003@gmail.com>

---
+++ rustaceaning/target/debug/deps/rustaceaning-f85f048ee3fa3e91.d
@@ -0,0 +1,7 @@
+/Users/ianmiller/GitHub/rustaceaning/rustaceaning/target/debug/deps/rustaceaning-f85f048ee3fa3e91.d: src/lib.rs
+
+/Users/ianmiller/GitHub/rustaceaning/rustaceaning/target/debug/deps/librustaceaning-f85f048ee3fa3e91.rlib: src/lib.rs
+
+/Users/ianmiller/GitHub/rustaceaning/rustaceaning/target/debug/deps/librustaceaning-f85f048ee3fa3e91.rmeta: src/lib.rs
+
+src/lib.rs:
---
+++ rustaceaning/target/debug/libbinary_search.d
@@ -0,0 +1,1 @@
+/Users/ianmiller/GitHub/rustaceaning/rustaceaning/target/debug/libbinary_search.rlib: /Users/ianmiller/GitHub/rustaceaning/rustaceaning/binary-search/src/lib.rs
